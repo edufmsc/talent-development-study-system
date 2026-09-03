d9bfc0a2998661beb366206e29b6259c0553879a
edufmsc <edu.fmsc@gmail.com>
2026-09-03 17:03:51 +0800

    Connect Day1 exact slide previews through S009

--- data/slide_media.js
+++ data/slide_media.js
@@ -5,6 +5,11 @@ window.SLIDE_MEDIA={
     'd1-s001':{driveFileId:'1iyhwSoQiI-avz8h5wsWVgoNXmEwRlCLJ',fileName:'D1_S001.jpg'},
     'd1-s002':{driveFileId:'1b0T2G16jD2ZVcvZiEh0I2hqkuTYvIBDr',fileName:'D1_S002.jpg'},
     'd1-s003':{driveFileId:'1TsCe5t-MdYFOoMQYvfjTaSIEvrnj2VOG',fileName:'D1_S003.jpg'},
-    'd1-s004':{driveFileId:'16Z0ZWkys5gSjGg5fgmOx8BhsSlBKsY4Z',fileName:'D1_S004.jpg'}
+    'd1-s004':{driveFileId:'16Z0ZWkys5gSjGg5fgmOx8BhsSlBKsY4Z',fileName:'D1_S004.jpg'},
+    'd1-s005':{driveFileId:'14LX6Cm-9i6z-rMYHp5ovV2F9wT384G_0',fileName:'D1_S005.jpg'},
+    'd1-s006':{driveFileId:'12C_juax8kvwOMFKf9EBZV2aLt8oA9UHq',fileName:'D1_S006.jpg'},
+    'd1-s007':{driveFileId:'1dPdxCnwve4cnvh-T4Pyf9wAZ9qJlkKw6',fileName:'D1_S007.jpg'},
+    'd1-s008':{driveFileId:'1lW0j1wH0OUkcCSfmgqM1wLzPtKCm1qb3',fileName:'D1_S008.jpg'},
+    'd1-s009':{driveFileId:'13tvCDJFci24YqAwqoPYE19M-ZEsHlhdG',fileName:'D1_S009.jpg'}
   }
 };
